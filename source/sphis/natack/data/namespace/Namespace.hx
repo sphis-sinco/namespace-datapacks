7db919c7c87c0af4eda919260e1e87d9183946dc
Viwu <sinconsistencia@gmail.com>
2025-10-18 22:30:46 -0500

    asset functions for n...

--- source/sphis/natack/data/namespace/Namespace.hx
+++ source/sphis/natack/data/namespace/Namespace.hx
@@ -1,5 +1,7 @@
 package sphis.natack.data.namespace;
 
+import sphis.natack.assets.NamespacePaths;
+
 class Namespace
 {
 	/**
@@ -11,4 +13,16 @@ class Namespace
 	{
 		this.id = id;
 	}
+
+	public function getAsset(asset:String):String
+		return NamespacePaths.getAsset(asset, this.id);
+
+	public function getDataTextAsset(asset:String):String
+		return getAsset('data/$asset.txt');
+
+	public function getDataJsonAsset(asset:String):String
+		return getAsset('data/$asset.json');
+
+	public function getImageAsset(asset:String):String
+		return getAsset('images/$asset.png');
 }
