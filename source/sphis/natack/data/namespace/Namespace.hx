7b9c02770c9ce3988f836fe765d75a342773aaf1
Viwu <sinconsistencia@gmail.com>
2025-10-18 22:20:53 -0500

    Namespace ID

--- source/sphis/natack/data/namespace/Namespace.hx
+++ source/sphis/natack/data/namespace/Namespace.hx
@@ -1,3 +1,14 @@
 package sphis.natack.data.namespace;
 
-class Namespace {}
+class Namespace
+{
+	/**
+	 * A unique namespace id that will also be the namespace folder name
+	 */
+	public var id:String;
+
+	public function new(id:String)
+	{
+		this.id = id;
+	}
+}
